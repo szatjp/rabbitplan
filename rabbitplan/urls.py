78f2052b0368c81cd91d3fbd6e40a40a934165b4
sunny <szxatjp@163.com>
2018-03-14 00:07:47 +0800

    dict edit

--- rabbitplan/urls.py
+++ rabbitplan/urls.py
@@ -1,8 +1,29 @@
+# coding:utf-8
+
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.views import login,logout
+
+from rest_framework import serializers, viewsets, routers
 
 from welcome.views import index, health
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     # Examples:
@@ -12,6 +33,12 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^dict/', include('dictdata.urls')),   # ×ÖµäÄ£¿é
+]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
 
 if settings.DEBUG:
