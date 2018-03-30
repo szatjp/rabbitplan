15088507e9269866a7a3d4d3870bd8e6876570ea
szatjp <szxatjp@163.com>
2018-03-30 13:10:10 +0800

    database config change

--- rabbitplan/database.py
+++ rabbitplan/database.py
@@ -11,14 +11,8 @@ engines = {
 
 
 def config():
-    service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-    if service_name:
-        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
-    else:
-        engine = engines['sqlite']
-    name = os.getenv('DATABASE_NAME')
-    if not name and engine == engines['sqlite']:
-        #name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
+    hostname = os.getenv('HOSTNAME', 'unknown')
+    if hostname=='unknown':
         return {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'rabbitdb',
@@ -26,12 +20,21 @@ def config():
             'PASSWORD': '123321',                  # Not used with sqlite3.
             'HOST': 'localhost',
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }        
-    return {
-        'ENGINE': engine,
-        'NAME': name,
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
-        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
-    }
+        }
+    else:      
+        service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
+        if service_name:
+            engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
+        else:
+            engine = engines['sqlite']
+        name = os.getenv('DATABASE_NAME')
+        if not name and engine == engines['sqlite']:
+            name = os.path.join(settings.BASE_DIR, 'db.sqlite3')      
+        return {
+            'ENGINE': engine,
+            'NAME': name,
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+            'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
+            'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
+        }
