62f96f3277077fc11fda2c1ba441eacd2fafff66
szatjp <szxatjp@163.com>
2018-05-23 12:37:26 +0800

    adminsite change

--- commonmd/admin.py
+++ commonmd/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from commonmd.models import ProCode
 
 # Register your models here.
+
+
+#admin.site.register(ProCode)
+
+@admin.register(ProCode)
+class ProCodeAdmin(admin.ModelAdmin):
+    list_display = ('fstateid', 'fstatename', 'fnexttime')
+    ordering = ['fstateid']
