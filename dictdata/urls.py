30c2f576626f96a6039919c8df08f9cd09d08ab3
szatjp <szxatjp@163.com>
2018-07-05 08:58:21 +0800

    jalist

--- dictdata/urls.py
+++ dictdata/urls.py
@@ -11,6 +11,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from dictdata.dictedit.dataopre import JpList,JpDetail,CnList,CnDetail,EnList,EnDetail
 from dictdata.cndict.cnmaintain import CnWordLi,CnWordCreate,CnWordUpdate
+from dictdata.jpdict.jamaintain import JaWordLi,JaWordCreate,JaWordUpdate
 
 urlpatterns = [
     path('jpwords/', JpList.as_view()),
@@ -22,7 +23,10 @@ urlpatterns = [
     path('cnwordlist/', CnWordLi.as_view()),
     path('cnword/add/', CnWordCreate.as_view(), name='cnword-add'),
     path('cnword/<int:pk>/', CnWordUpdate.as_view(), name='cnword-update'),
-    #path('cnword/<int:pk>/delete/', CnWordDelete.as_view(), name='cnword-delete'),             
+    #path('cnword/<int:pk>/delete/', CnWordDelete.as_view(), name='cnword-delete'),
+    path('jawordlist/', JaWordLi.as_view()),
+    path('jaword/add/', JaWordCreate.as_view(), name='jaword-add'),
+    path('jaword/<int:pk>/', JaWordUpdate.as_view(), name='jaword-update'),             
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
