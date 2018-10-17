b9e44e00c8b83df1a338b0ea4aec0e30b5d42a72
szatjp <szxatjp@163.com>
2018-10-17 17:03:07 +0800

    change

--- dictdata/cndict/cnmaintain.py
+++ dictdata/cndict/cnmaintain.py
@@ -202,6 +202,7 @@ class CnWordUpdate(UpdateView):
     model = CnWord
     template_name = 'dictedit/wordedit.html'
     fields = ['fword','fpronunciation','fwordclass']
+    #success_url = request.META.get('HTTP_REFERER')
     def get_context_data(self, **kwargs):
         context = super(CnWordUpdate, self).get_context_data(**kwargs)
         tranluan = []
