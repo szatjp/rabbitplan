3113be63055b80e252dd0a842046a961dc097fd2
szatjp <szxatjp@163.com>
2018-11-01 10:44:39 +0800

    trans download

--- commonmd/datatrans/datadown.py
+++ commonmd/datatrans/datadown.py
@@ -15,7 +15,7 @@ from django.http import HttpResponse
 from commonfunc.dbtojson import json_encode
 
 from commonmd.models import FreshTime
-from dictdata.models import JaWord,EnWord,CnWord,Ja2Cn
+from dictdata.models import JaWord,EnWord,CnWord,Ja2Cn,Ja2En,En2Cn
 
 
 # 下载字典
@@ -72,7 +72,7 @@ def downfunc(url,dbmodel):
     return 'success'
 
 # 下载互译表
-def downtrans(url,dbmodel):
+def downtrans(url,trantype):
     #url = 'http://django-psql-persistent-rabbitplan.193b.starter-ca-central-1.openshiftapps.com/dict/jpwords/'
     #url = 'http://127.0.0.1:8000/dict/jpwords/'
 
@@ -100,26 +100,17 @@ def downtrans(url,dbmodel):
     html = response.read().decode('utf-8')
     jsondata = json.loads(html)
     objli = []
-    for dictdata in jsondata:
-        if dbmodel.objects.filter(pk=dictdata['fwordno']).exists():
-            wordobj = dbmodel.objects.get(pk=dictdata['fwordno'])
-            wordobj.fword = dictdata['fword']
-            wordobj.fpronunciation = dictdata['fpronunciation']
-            wordobj.fwordclass = dictdata['fwordclass']
-            wordobj.frelaword = dictdata['frelaword']
-            wordobj.fuser = dictdata['fuser']
-            wordobj.save()              
-        else:    
-            dictobj = dbmodel(
-                fwordno = dictdata['fwordno'],
-                fword = dictdata['fword'],
-                fpronunciation = dictdata['fpronunciation'],
-                fwordclass = dictdata['fwordclass'],
-                frelaword = dictdata['frelaword'],
-                fuser = dictdata['fuser']          
-                )
-            objli.append(dictobj)
-    dbmodel.objects.bulk_create(objli)
+    if trantype=="ja2cn":
+        for dictdata in jsondata:
+            if not Ja2Cn.objects.filter(fjaword=dictdata['fjaword'],fcnword=dictdata['fcnword']).exists():    
+                dictobj = Ja2Cn(
+                    fjaword = dictdata['fjaword'],
+                    fcnword = dictdata['fcnword'],
+                    fuser = dictdata['fuser'],
+                    fdate = dictdata['fdate']        
+                    )
+                objli.append(dictobj)
+        Ja2Cn.objects.bulk_create(objli)
     freshdayobj.ffreshtime = datetime.datetime.now()
     freshdayobj.save()
     return 'success'
@@ -134,7 +125,15 @@ def downdict(request):
     # 下载中文字典
     cnurl = 'http://django-psql-persistent-rabbitplan.193b.starter-ca-central-1.openshiftapps.com/dict/cnwords/'
     downfunc(cnurl,CnWord)    
-    
+    # 日中互译表
+    geturl = 'http://django-psql-persistent-rabbitplan.193b.starter-ca-central-1.openshiftapps.com/common/jctrans/'
+    downtrans(geturl,'ja2cn')
+    # 日英互译表
+    geturl = 'http://django-psql-persistent-rabbitplan.193b.starter-ca-central-1.openshiftapps.com/common/jetrans/'
+    downtrans(geturl,'ja2en')
+    # 英中互译表
+    geturl = 'http://django-psql-persistent-rabbitplan.193b.starter-ca-central-1.openshiftapps.com/common/ectrans/'
+    downtrans(geturl,'en2cn')        
     #url = 'http://127.0.0.1:8000/dict/jpwords/'
 
     #postData = urllib.parse.urlencode(data).encode('utf-8')
@@ -143,10 +142,22 @@ def downdict(request):
 
 # 日汉json
 def jcjson(request):
-    ja2cnobjs = Ja2Cn.objects.values('fjaword','fcnword','fdate').all()  
+    ja2cnobjs = Ja2Cn.objects.values('fjaword','fcnword','fuser','fdate').all()  
     jsondata = json_encode(ja2cnobjs)
     #url = 'http://127.0.0.1:8000/dict/jpwords/'
 
     #postData = urllib.parse.urlencode(data).encode('utf-8')
     #restreq = urllib.request.Request(url,postData,{'Content-Type': 'application/json'})
-    return HttpResponse(jsondata)  
+    return HttpResponse(jsondata)
+
+# 日汉json
+def jejson(request):
+    ja2enobjs = Ja2En.objects.values('fjaword','fenword','fuser','fdate').all()  
+    jsondata = json_encode(ja2enobjs)
+    return HttpResponse(jsondata) 
+
+# 英中json
+def ecjson(request):
+    en2cnobjs = En2Cn.objects.values('fenword','fcnword','fuser','fdate').all()  
+    jsondata = json_encode(en2cnobjs)
+    return HttpResponse(jsondata)   
--- commonmd/urls.py
+++ commonmd/urls.py
@@ -11,7 +11,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from dictdata.dictedit.dataopre import JpList,JpDetail,CnList,CnDetail
 from jpstudy.study import word
-from commonmd.datatrans.datadown import downdict,jcjson
+from commonmd.datatrans.datadown import downdict,jcjson,jejson,ecjson
 from commonmd.datatrans.dataup import upprocode,recprocode
 
 urlpatterns = [
@@ -19,6 +19,8 @@ urlpatterns = [
     #url(r'^cclist/$', 'jpstudy.study.learn.curvecodelist'),  # 记忆曲线状态代码列表
     path('downdict/', downdict),  # 下载词典
     path('jctrans/', jcjson),  # 下载Ja2Cn日汉互译
+    path('jetrans/', jejson),  # 下载Ja2En日英互译
+    path('ectrans/', ecjson),  # 下载EN2Cn英汉互译
     path('improcode/', upprocode),  # 上传记忆曲线状态代码
     path('recprocode/', recprocode),  # 接收记忆曲线状态代码
     #url(r'^jpwords/(?P<pk>[a-z0-9]+)/$', JpDetail.as_view()),
