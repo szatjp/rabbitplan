1559648638205b47e7dc73b682da64fbe9a8cb85
szatjp <szxatjp@163.com>
2018-12-18 18:21:36 +0800

    ck

--- userstudy/urls.py
+++ userstudy/urls.py
@@ -8,7 +8,7 @@ Created on 2016-1-18
 
 from django.urls import path,re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from dictdata.dictedit.dataopre import JpList,JpDetail,CnList,CnDetail
+from userstudy.words.tagedit import WordClassLi,WordClassCreate,WordClassDetail,WordClassUpdate
 from userstudy.words.newwords import searchword,findtonew
 from userstudy.words.newwords import NewWordLi
 
@@ -16,6 +16,10 @@ urlpatterns = [
     path('findword/', searchword),
     re_path(r'^addnew/(\w+)/(\w+)/$', findtonew),
     path('newwordli/', NewWordLi.as_view()),
+    path('wclass/', WordClassLi.as_view()),
+    path('wclass/add/', WordClassCreate.as_view(), name='wclass-add'),
+    path('wclass/<slug:pk>/', WordClassDetail.as_view(), name='wclass-detail'), 
+    path('wclass/<slug:pk>/update/', WordClassUpdate.as_view(), name='wclass-update'),     
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
